20c8d759c00fde3966093d252ba82d9f020a2bed
yair2000 <32851854+yair2000@users.noreply.github.com>
2019-04-02 14:35:12 +0300

    Update Done.java

--- app/src/main/java/com/yairnet/quizgame/Done.java
+++ app/src/main/java/com/yairnet/quizgame/Done.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -14,8 +13,7 @@ import com.yairnet.quizgame.model.QuestionScore;
 
 public class Done extends AppCompatActivity
 {
-    Button tryAgainBtn;
-    TextView totalScoreTxt, totalResultTxt;
+    TextView totalScoreTxt, totalResultTxt, tryAgainBtn;
 
     FirebaseDatabase database;
     DatabaseReference question_score;
@@ -31,7 +29,7 @@ public class Done extends AppCompatActivity
         // (R.id.text) - the "text" are ids from the "activity_done.xml)
         totalScoreTxt = (TextView)findViewById(R.id.totalScoreTxt);
         totalResultTxt = (TextView)findViewById(R.id.totalResultTxt);
-        tryAgainBtn = (Button)findViewById(R.id.tryAgainBtn);
+        tryAgainBtn = (TextView)findViewById(R.id.tryAgainBtn);
 
         tryAgainBtn.setOnClickListener(new View.OnClickListener(){
 
