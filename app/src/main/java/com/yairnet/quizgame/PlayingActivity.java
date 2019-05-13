dd3e59767c02c28ffb38e9ccd3232dfb73cc3706
yair2000 <32851854+yair2000@users.noreply.github.com>
2019-05-13 18:15:21 +0300

    Update PlayingActivity.java

--- app/src/main/java/com/yairnet/quizgame/PlayingActivity.java
+++ app/src/main/java/com/yairnet/quizgame/PlayingActivity.java
@@ -1,6 +1,7 @@
 package com.yairnet.quizgame;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
