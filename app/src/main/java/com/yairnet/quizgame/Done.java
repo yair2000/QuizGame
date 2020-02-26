4749d097fec8b9662bfb84c4af88a68197e41c01
yair2000 <32851854+yair2000@users.noreply.github.com>
2020-02-26 22:15:47 +0200

    Update Done.java
    Android X imported

--- app/src/main/java/com/yairnet/quizgame/Done.java
+++ app/src/main/java/com/yairnet/quizgame/Done.java
@@ -1,7 +1,7 @@
 package com.yairnet.quizgame;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
