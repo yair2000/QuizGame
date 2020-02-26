35f932be4f45454ca709a15657bac4a88c5c9af3
yair2000 <32851854+yair2000@users.noreply.github.com>
2020-02-26 22:17:51 +0200

    Update ScoreDetail.java
    Android X imported

--- app/src/main/java/com/yairnet/quizgame/ScoreDetail.java
+++ app/src/main/java/com/yairnet/quizgame/ScoreDetail.java
@@ -1,9 +1,9 @@
 package com.yairnet.quizgame;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
